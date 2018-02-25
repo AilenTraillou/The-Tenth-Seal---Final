86db14ebd844a6022e081531417d6e77a728bcce
AilenTraillou <ailentraillou@gmail.com>
2018-02-25 08:43:13 -0300

    Revert "Mecanismo de Cuchillas - Hospital"
    This reverts commit 9d37066943c1c9aad3d2a3da4a3364e357ae0221.

--- The Tenth Seal/Assets/Scripts/UI/LevelManager.cs
+++ The Tenth Seal/Assets/Scripts/UI/LevelManager.cs
@@ -7,13 +7,6 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 
-    public event Action PassNextLevel = delegate { };
-    public event Action NoMedicineUsedAchievement = delegate { };
-    public event Action AllItemsFoundOnLevel1= delegate { };
-    public event Action TimeRecordOnLevel1 = delegate { };
-    public event Action NoOilCharged = delegate { };
-    public event Action LessDamageTaken = delegate { };
-
     public Transform character;
     public Transform level;
     public int sceneNumber;
@@ -23,42 +16,11 @@ public class LevelManager : MonoBehaviour {
     bool _onPause;
     ModelCharacter model;
     GameManager gameManager;
-    Achievements achievements;
-
-    static bool noMedicineUsedAchivOnLevel1;
-    static bool allItemsFoundOnLevel1;
-    static bool timeRecordOnLevel1;
-    static bool noOilCharged;
-    static bool lessDamageTaken;
 
     void Start()
     {
         model = FindObjectOfType<ModelCharacter>();
         gameManager = FindObjectOfType<GameManager>();
-        achievements = FindObjectOfType<Achievements>();
-
-
-        PassNextLevel += model.SetStatsOnNextLevel;
-        NoMedicineUsedAchievement += achievements.NoMedicineAchievement;
-        AllItemsFoundOnLevel1 += achievements.AllItemsFoundOnLevel1;
-        TimeRecordOnLevel1 += achievements.TimeRecordONLevel1;
-        TimeRecordOnLevel1 += achievements.NoOilCharged;
-        TimeRecordOnLevel1 += achievements.LessDamageTaken;
-
-        if (noMedicineUsedAchivOnLevel1)
-            NoMedicineUsedAchievement();
-
-        if (allItemsFoundOnLevel1)
-            AllItemsFoundOnLevel1();
-
-        if (timeRecordOnLevel1)
-            TimeRecordOnLevel1();
-
-        if (noOilCharged)
-            NoOilCharged();
-
-        if (lessDamageTaken)
-            LessDamageTaken();
     }
 
     void Update()
@@ -88,7 +50,7 @@ public class LevelManager : MonoBehaviour {
             {"Level", _scene.name},
             {"Remaining Life", model.life},
             {"Remaining Oil", Math.Truncate(model.oil)},
-            {"Total Consumed Oil",  Math.Truncate(model.totalConsumedOil)},
+            {"Total Consumed Oil",  model.totalConsumedOil},
             {"Total Damage Taken",  model.totalDamage},
 
         });
@@ -107,8 +69,6 @@ public class LevelManager : MonoBehaviour {
         {
             SceneManager.LoadScene(sceneNumber);
             LevelSuccessStatistics();
-            PassNextLevel();
-            FinishGameAchievements();
         }
     }
 
@@ -127,34 +87,6 @@ public class LevelManager : MonoBehaviour {
     public void OnPause(bool onPause)
     {
         _onPause = onPause;
-    }
-
-    public void FinishGameAchievements()
-    {
-        Scene _scene = SceneManager.GetActiveScene();
-
-        if (ObjectsCount.instance.lifeRecovered == 0)
-            noMedicineUsedAchivOnLevel1 = true;
-
-        if (ObjectsCount.instance.totalItemsOnLevelFound == ObjectsCount.instance.itemsOnLevel1
-        && _scene.buildIndex == 3)     
-            allItemsFoundOnLevel1 = true;
-
-        if (ObjectsCount.instance.totalItemsOnLevelFound == ObjectsCount.instance.itemsOnLevel1
-        && _scene.buildIndex == 3)
-            allItemsFoundOnLevel1 = true;
-
-
-        if (timeElapsed <= 120)
-            timeRecordOnLevel1 = true;
-
-        if (ObjectsCount.instance.totalOilCharged == 0 && ObjectsCount.instance.lifeRecovered == 0)
-            noOilCharged = true;
-
-        if (ObjectsCount.instance.damageTaken <= 30)
-            lessDamageTaken = true;
-
-        
     }
 
 }
