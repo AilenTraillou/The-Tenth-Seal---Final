7f9028b1d920513fd94c082ae722ec071f22b617
AilenTraillou <ailentraillou@gmail.com>
2018-02-25 15:24:51 -0300

    Cuchillas nivel hospital v2

---
+++ The Tenth Seal/Assets/Scripts/Enemys/Blade.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blade : MonoBehaviour {
+
+	void Start()
+    {
+		    
+	}
+	
+	void Update()
+    {
+        transform.Rotate(Vector3.up * 1000 * Time.deltaTime, Space.Self);
+    }
+}
---
+++ The Tenth Seal/Assets/Scripts/Enemys/BladeMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BladeMovement : Enemy {
+
+    public float degrees;
+    public float radians;
+    public float radiusX;
+    public float radiusY;
+    public float speedRotation;
+
+    public GameObject prefabBullet;
+    public Transform center;
+
+    public int numBullets;
+    public int invokeTime;
+
+    public float a;
+    public float b;
+    public float h;
+    bool invokeBlade;
+
+    void Start()
+    {
+        Invoke("InvokeBlade", invokeTime);
+
+        if (gameObject.tag == "fullBlade" || gameObject.tag == "verticalBlade")
+            damage = 10;
+        else
+            damage = 0;
+    }
+
+    void Update () {
+
+        if (invokeBlade)
+        {
+            degrees += speedRotation * Time.deltaTime;
+            radians = degrees * Mathf.Deg2Rad;
+
+            if (gameObject.tag == "fullBlade")
+            {
+
+                Vector3 posInEpitrocoide = transform.position;
+                posInEpitrocoide.x = posInEpitrocoide.x + ((a - b) * Mathf.Cos(radians) - h * Mathf.Cos(((a + b) / b) * radians ));
+                posInEpitrocoide.y = posInEpitrocoide.y + ((a - b) * Mathf.Sin(radians) - h * Mathf.Sin(((a + b) / b) * radians));
+
+                transform.position = posInEpitrocoide;
+            }
+
+            if(gameObject.tag == "verticalBlade")
+            {
+                Vector3 posInEpitrocoide = transform.position;
+                posInEpitrocoide.y = posInEpitrocoide.y + ((a - b) * Mathf.Sin(radians) - h * Mathf.Sin(((a + b) / b) * radians));
+
+                transform.position = posInEpitrocoide;
+            }
+        }
+    }
+
+    void InvokeBlade()
+    {
+        invokeBlade = true;
+    }
+}
